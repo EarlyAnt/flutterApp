cbd420a3a28225bb6e0293692535bec1150b1a82
EarlyAnt <54763755@qq.com>
2020-07-03 05:41:12 +0800

    1.调整了底部导航栏文字和图标的颜色。 2.顶部导航栏改为Scaffold中，TabBar的底色与AppBar融为一体了。

--- flutterapp/lib/pages/tabs/HomePage.dart
+++ flutterapp/lib/pages/tabs/HomePage.dart
@@ -1,55 +1,75 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key: key);
-
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black26,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: TabBar(
-                  isScrollable: true, //如果多个按钮的话可以滑动
-                  indicatorColor: Colors.yellow,
-                  labelColor: Colors.yellow,
-                  unselectedLabelColor: Colors.white,
-                  indicatorSize: TabBarIndicatorSize.label,
-
-                  tabs: <Widget>[
-                    Tab(text: "我的饭盒"),
-                    Tab(text: "菜谱"),
-                  ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: choices.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: TabBar(
+                    isScrollable: true,
+                    tabs: choices.map((Choice choice) {
+                      return Tab(
+                        text: choice.title,
+                        icon: Icon(choice.icon),
+                      );
+                    }).toList(),
+                  ),
                 ),
-              )
-            ],
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: choices.map((Choice choice) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ChoiceCard(choice: choice),
+              );
+            }).toList(),
           ),
         ),
-        body: TabBarView(
+      ),
+    );
+  }
+}
+
+class Choice {
+  const Choice({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: '我的饭盒', icon: Icons.fastfood),
+  const Choice(title: '菜谱', icon: Icons.book),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.headline4;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            ListView(
-              children: <Widget>[
-                ListTile(title: Text("第一个tab")),
-                ListTile(title: Text("第一个tab")),
-                ListTile(title: Text("第一个tab"))
-              ],
-            ),
-            ListView(
-              children: <Widget>[
-                ListTile(title: Text("第二个tab")),
-                ListTile(title: Text("第二个tab")),
-                ListTile(title: Text("第二个tab"))
-              ],
-            ),
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
           ],
         ),
       ),
