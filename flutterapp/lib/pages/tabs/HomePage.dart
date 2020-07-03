f8e969296fac6aeb7fa5bfdcf620c678929628e0
EarlyAnt <54763755@qq.com>
2020-07-03 18:33:18 +0800

    1.主页->我的饭盒页签，使用GridView组件实现了列表布局，使用Card组件实现了每个Item的外观，包括icon，title和number。

--- flutterapp/lib/pages/tabs/HomePage.dart
+++ flutterapp/lib/pages/tabs/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class HomePage extends StatelessWidget {
   @override
@@ -67,15 +68,88 @@ class ChoiceCard extends StatelessWidget {
     return Card(
       color: Colors.white,
       child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+            childAspectRatio: 0.9,
+          ),
+          children: dishes.map((dish) {
+            return DishCard(choice: dish);
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+const List<Choice> dishes = const <Choice>[
+  const Choice(title: '饭盒1', icon: Icons.fastfood),
+  const Choice(title: '饭盒2', icon: Icons.book),
+  const Choice(title: '饭盒3', icon: Icons.cloud),
+  const Choice(title: '饭盒4', icon: Icons.fastfood),
+  const Choice(title: '饭盒5', icon: Icons.book),
+  const Choice(title: '饭盒6', icon: Icons.cloud),
+  const Choice(title: '饭盒7', icon: Icons.fastfood),
+  const Choice(title: '饭盒8', icon: Icons.book),
+  const Choice(title: '饭盒9', icon: Icons.cloud),
+  const Choice(title: '饭盒10', icon: Icons.fastfood),
+  const Choice(title: '饭盒11', icon: Icons.book),
+  const Choice(title: '饭盒12', icon: Icons.cloud),
+  const Choice(title: '饭盒13', icon: Icons.fastfood),
+  const Choice(title: '饭盒14', icon: Icons.book),
+  const Choice(title: '饭盒15', icon: Icons.cloud),
+];
+
+class DishCard extends StatelessWidget {
+  const DishCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.headline4;
+    return Card(
+      color: Colors.lightBlue[50],
+      child: Center(
+        child: Stack(
           children: <Widget>[
-            Icon(choice.icon, size: 128.0, color: textStyle.color),
-            Text(choice.title, style: textStyle),
+            Align(
+              alignment: Alignment.center,
+              child: Icon(choice.icon, size: 64),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(choice.title),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 18.0,
+                height: 18.0,
+                child: CircleAvatar(
+                  backgroundColor: _getColor(),
+                  child: Text(
+                    Random().nextInt(10).toString(),
+                    style: TextStyle(fontSize: 12, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+Color _getColor() {
+  int value = Random().nextInt(10) % 3;
+  if (value == 0)
+    return Colors.red[300];
+  else if (value == 1)
+    return Colors.orange[300];
+  else
+    return Colors.green[300];
 }
